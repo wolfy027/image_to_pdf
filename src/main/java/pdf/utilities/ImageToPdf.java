f9e8d16d1eb5fcdb471d0f4636234b627f506785
wolfy027 <jomon027@gmail.com>
2024-06-06 23:21:56 +0200

    src change for file structure change

--- src/main/java/pdf/utilities/ImageToPdf.java
+++ src/main/java/pdf/utilities/ImageToPdf.java
@@ -1,13 +1,16 @@
 package pdf.utilities;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -17,13 +20,22 @@ public class ImageToPdf {
 
     public static void main(String[] arg) throws Exception {
         File root = new File("./setup/input");
-        String outputFile = "output.pdf";
-        File[] files = root.listFiles();
-        Arrays.sort(files, new FilenameComparator());
+        String outputDirectory = "./setup/output";
+        File[] inputFolders = root.listFiles(File::isDirectory);
+        for (File inputDirectory : Objects.requireNonNull(inputFolders)) {
+            String directoryName = inputDirectory.getName();
+            String outputFileName = directoryName.concat(".pdf");
+            makePdfFromFolder(inputDirectory, outputDirectory, outputFileName);
+        }
+    }
+
+    private static void makePdfFromFolder(File inputDirectory, String outputDirectory, String outputFileName) throws DocumentException, IOException {
+        File[] files = inputDirectory.listFiles();
+        Arrays.sort(Objects.requireNonNull(files), new FilenameComparator());
         logger.log(Level.INFO, "File count : {0}", files.length);
 
         Document document = new Document();
-        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(new File("./setup/output", outputFile)));
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(new File(outputDirectory, outputFileName)));
         writer.setFullCompression();
         document.open();
         for (File file : files) {
@@ -37,7 +49,10 @@ public class ImageToPdf {
             document.add(image);
         }
         document.close();
-        PdfUtils.compressPdfWithPdfBox("./setup/output/output.pdf", "./setup/output/output-compressed.pdf");
+        PdfUtils.compressPdfWithPdfBox(
+                outputDirectory.concat("/").concat(outputFileName),
+                outputDirectory.concat("/").concat(inputDirectory.getName()).concat("-compressed.pdf")
+        );
     }
 
 }
